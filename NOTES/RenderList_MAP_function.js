a084f1657ca2305f0a874673349dc01fbf41cf42
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-05-02 21:21:25 +0530

    Render List to

---
+++ NOTES/RenderList_MAP_function.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+//components
+import Person from './Person/Person';
+
+class App extends Component{
+
+  state = {
+    persons: [
+      {name: 'Karan', age: 26},
+      {name: 'Kajol', age: 27}
+    ],
+    showPersons: true
+  }; 
+
+  switchNameHandler = () => {
+    // DONT DO THIS: this.state.persons[0].name = 'Missisipi';
+    //It will merge original and not touch anything else
+    this.setState({
+      persons:[
+        {name: 'Narak', age: 62},
+        {name: 'Lojak', age: 72}
+      ]
+    });
+  };
+
+  onChangeHandler = (input) => {
+    this.setState({
+      persons:[
+        {name: input.target.value, age: 62},
+        {name: 'Lojak', age: 62}
+      ]
+    });
+  }; 
+
+  togglePersonHandler = () => {
+    this.setState({
+      showPersons: this.state.showPersons ? false : true
+    });
+  };
+
+  render() {
+
+    //inline CSS
+    const style = {
+      backgroundColor: 'teal',
+      font: 'inherit',
+      border: '1px solid aquamarine',
+      padding: '8px',
+      color: 'white',
+      borderRadius: '3px',
+      cursor: 'pointer'
+    }
+//////////////////////////////////////////////////////////
+    let persons = null;
+    //Since render function always runs
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person) => {
+            return <Person 
+              name={person.name}
+              age={person.age}/>
+          })}
+        </div>
+      );
+    } 
+
+    return (
+      <div className="App">
+        
+        <h1>You're seeing me from Root Component!</h1>
+        <button 
+          onClick={this.togglePersonHandler}
+          style={style}>
+          Switch Names
+        </button>
+
+        {persons}
+
+    </div>     
+    );
+  }
+}
+//////////////////////////////////////////////////////////
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi there, you miss me?</h1>
+//       <Person age="28"> My age is </Person>
+//       <Person age="35"> My age is </Person>
+//       <Person age="19"> My age is </Person>
+//     </div>
+//   );
+// }
+
+export default App;
